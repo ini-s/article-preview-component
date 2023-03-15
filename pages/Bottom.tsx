bdcd0158d31170b1a8996d9a63d8fbe1d0b2313c
IMS <inisoetan2014@gmail.com>
2023-03-15 09:25:13 +0000

    add active state

--- pages/Bottom.tsx
+++ pages/Bottom.tsx
@@ -1,71 +1,88 @@
 import styles from "../styles/Bottom.module.css"
 import Image from 'next/image'
+import { useState } from "react"
+
 interface Props {
     author: string;
     date: string;
 }
+
 export default function Bottom({ author, date }: Props) {
+    const [active, setIsActive] = useState<boolean>(false)
+    const bottomStyle = {
+        backgroundColor: active ? "hsl(217, 19%, 35%)" : "white",
+        padding: active ? "0" : "1rem 2rem 1.5rem 2rem"
+    }
+
+    function handleClick() {
+        setIsActive(prevValue => !prevValue)
+    }
     return (
-        <div className={styles.bottom}>
-            <Image
-                className={styles.avatar}
-                src="/images/avatar-michelle.jpg"
-                alt="avatar-img"
-                width={175}
-                height={175}
-            />
-            <div className={styles.bottomInfo}>
-                <p className={styles.author}>
-                    {author}
-                </p>
-                <p className={styles.date}>
-                    {date}
-                </p>
-            </div>
-            <div className={styles.shareIconCircle}>
-                <Image
-                    className={styles.shareIcon}
-                    src="/images/icon-share.png"
-                    alt="share-image"
-                    width={15}
-                    height={13}
-                />
-            </div>
-            {/* <div className={styles.activeBottom}>
-                <p className={styles.share}>
-                    share
-                </p>
-                <Image
-                    className={styles.icon}
-                    src="/images/icon-facebook.png"
-                    alt="facebook-image"
-                    width={20}
-                    height={20}
-                />
-                <Image
-                    className={styles.icon}
-                    src="/images/icon-twitter.png"
-                    alt="twitter-image"
-                    width={20}
-                    height={17}
-                />
-                <Image
-                    className={styles.icon}
-                    src="/images/icon-pinterest.png"
-                    alt="pinterest-image"
-                    width={20}
-                    height={20}
-                />
-                <div className={styles.shareIconCircleActive}>
+        <div className={styles.bottom} style={bottomStyle}>
+            {!active ?
+                <>
                     <Image
-                        className={styles.shareIconActiv}
-                        src="/images/icon-share.png"
-                        alt="share-image"
-                        width={15}
-                        height={13}
+                        className={styles.avatar}
+                        src="/images/avatar-michelle.jpg"
+                        alt="avatar-img"
+                        width={175}
+                        height={175}
                     />
+                    <div className={styles.bottomInfo}>
+                        <p className={styles.author}>
+                            {author}
+                        </p>
+                        <p className={styles.date}>
+                            {date}
+                        </p>
+                    </div>
+                    <div className={styles.shareIconCircle} onClick={handleClick}>
+                        <Image
+                            className={styles.shareIcon}
+                            src="/images/icon-share.png"
+                            alt="share-image"
+                            width={15}
+                            height={13}
+                        />
+                    </div>
+                </>
+                :
+                <div className={styles.activeBottom}>
+                    <p className={styles.share}>
+                        share
+                    </p>
+                    <Image
+                        className={styles.icon}
+                        src="/images/icon-facebook.png"
+                        alt="facebook-image"
+                        width={20}
+                        height={20}
+                    />
+                    <Image
+                        className={styles.icon}
+                        src="/images/icon-twitter.png"
+                        alt="twitter-image"
+                        width={20}
+                        height={17}
+                    />
+                    <Image
+                        className={styles.icon}
+                        src="/images/icon-pinterest.png"
+                        alt="pinterest-image"
+                        width={20}
+                        height={20}
+                    />
+                    <div className={styles.shareIconCircleActive} onClick={handleClick}>
+                        <Image
+                            className={styles.shareIconActiv}
+                            src="/images/icon-share.png"
+                            alt="share-image"
+                            width={15}
+                            height={13}
+                        />
+                    </div>
                 </div>
-            </div> */}
+            }
         </div >
     )
 }
